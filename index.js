d9ab963228bf5f302c088015a3e48971d8e6c2a4
mynamesbao11 <baonguyenduy2002@gmail.com>
2023-01-02 22:38:12 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const db = mysql.createConnection({
+  host: "containers-us-west-62.railway.app",
+  user: "root",
+  port: "6378",
+  password: "QQgD8tSGMXWPsUemEOW4",
+  database: "railway",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Error connecting to Database", err);
+    return;
+  }
+  console.log("Connection established");
+});
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded());
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM railway.order_";
+  db.query(sqlSelect, (error, result) => {
+    console.log(result);
+  });
+});
+
+//------------IP => localhost:...--------------------------
+app.listen(PORT, () => {
+  console.log("Server started on port ", PORT, "!");
+});
