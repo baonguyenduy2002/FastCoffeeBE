b753c3f3234a98746e4378d9c46d608dda62faf6
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2023-01-05 19:54:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.post("/api/customer/signup", (req, res) => {
 app.post("/api/customer/get/order_history", (req, res) => {
   const sqlSelect = (
     "SELECT Order_ID, DateTime, Status, Order_note, Name, Address" + 
-    "FROM railway.order_ INNER JOIN railway.shop " + 
+    "FROM railway.order_ INNER JOIN railway.shop" + 
     "ON railway.order_.Shop_ID = railway.shop.Shop_ID WHERE railway.order_.Customer_ID = ?"
   );
   db.query(sqlSelect, [req.body.id], (error, result) => {
